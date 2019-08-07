bfd80b3d8c0409281a575423c73d83ca387c843b
David Kilson <itzkwaysi@gmail.com>
2019-08-07 16:01:28 +0100

    Fixed header to show user

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { connect } from 'react-redux';
 import './css/Header.css';
 
 class Header extends Component {
-  render () {
-    return (
-      <nav className="header">
-        <div>
-          <a href="/home">Haven</a>
+  showMenu () {
+    const nav = (
+      <ul className="menu">
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+      </ul>
+    );
+
+    const user = (
+      <>
+        <div className="user">
+          <p>name</p>
+          <ul>
+            <a href="/login"><li><FontAwesomeIcon icon={faSignOutAlt} /> Logout</li></a>
+          </ul>
         </div>
-        <ul>
-          <li><a href="/login">Login</a></li>
-          <li><a href="/register">Register</a></li>
-        </ul>
-      </nav>
+      </>
+    );
+
+    return this.props.isLoggedIn ? user : nav;
+  }
+
+  render () {
+    const show = this.showMenu();
+    return (
+        <nav className="header">
+          <div className="logo">
+            <a href="/home">Haven</a>
+          </div>
+          {show}
+        </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  const {isLoggedIn} = state.Auth;
+  return {
+    isLoggedIn
+  }
+}
+
+export default connect(mapStateToProps, {})(Header);
