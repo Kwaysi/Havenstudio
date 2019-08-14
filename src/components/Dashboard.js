bbc2644a78b9c04765da194219e4354ffb31264e
David Kilson <itzkwaysi@gmail.com>
2019-08-14 11:24:48 +0100

    css fixes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -94,7 +94,7 @@ class Dashboard extends Component {
             <h1>Previous Bookings</h1>
             {prevBooking}
           </div>
-            <Footer/>
+          <Footer/>
         </div>
       </>
     );
